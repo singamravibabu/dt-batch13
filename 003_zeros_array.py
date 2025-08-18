cb497d043f0caa213f5bb01bbde90c81d4615b52
Singam Ravi Babu <Singam Ravi Babu>
2025-08-18 13:02:21 +0530

    003 file added

---
+++ 003_zeros_array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.zeros((5, 10), dtype=int)
+
+print(arr)
+print("Array attributes:")
+print("Shape:", arr.shape)
+print("Dimensions:", arr.ndim)
+print("Size:", arr.size)
+print("Data type:", arr.dtype)
