936b1cbce10bcaac51d049504933451e46ac0197
Singam Ravi Babu <Singam Ravi Babu>
2025-08-18 13:05:53 +0530

    004 is added

---
+++ 004_ones_array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.ones((3, 4), dtype=int)
+
+print(arr)
+print("Array attributes:")
+print("Shape:", arr.shape)
+print("Dimensions:", arr.ndim)
+print("Size:", arr.size)
+print("Data type:", arr.dtype)
